3957fd3c4a18a39ff4fea5fff603f42bc403c1cd
CarlosDaniel-090304 <carlosdanielchilcon19@gmail.com>
2026-02-04 10:45:47 -0500

    Creando controladores

---
+++ server/src/models/Product.js
@@ -0,0 +1,30 @@
+const mongoose = required('mongoose')
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'El nombre del producto es obligatorio'],
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: [true, 'El precio es obligatorio'],
+        min: 0
+    },
+    description: {
+        type: String,
+        required: [true, 'La descripcion es obligatoria']
+    },
+    imagen: {
+        type: String,
+        default: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJf9R0M__InKs1-YwTG0D-2xDHFbaQmywMOg&s'
+    },
+    category: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+})
+
+module.exports = mongoose.model('Product', productSchema)
---
+++ server/src/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    email: {
+        type: String,
+        required: [true, 'El correo es obligatorio'],
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es obligatoria']
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('User', userSchema)
